e05ef6799ccd48eedb890ddb7d840dcaf687dbae
Nutchi <nutchinet@outlook.com>
2023-05-04 16:07:14 +0900

    fix: PowerShell profile path

--- install.bat
+++ install.bat
@@ -16,4 +16,4 @@ set base=%~dp0
 
 call:mymklink "%USERPROFILE%\.gitconfig" "%base%\dotfiles\.gitconfig"
 call:mymklink "%APPDATA%\alacritty\alacritty.yml" "%base%\dotfiles\alacritty-win.yml"
-call:mymklink "%APPDATA%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "%base%\dotfiles\Microsoft.PowerShell_profile.ps1"
+call:mymklink "%USERPROFILE%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "%base%\dotfiles\Microsoft.PowerShell_profile.ps1"
